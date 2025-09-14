f1b456862a526326cb8f70e2da8a8f81517fddf4
JJ Milner <59690795+jjrmilner@users.noreply.github.com>
2025-09-14 09:44:26 +0200

    Update mfa-policycheck.ps1

--- mfa-policycheck.ps1
+++ mfa-policycheck.ps1
@@ -1,6 +1,14 @@
-<#
+<# SPDX-License-Identifier: Apache-2.0 WITH Commons-Clause
+# Copyright (c) 2025 Global Micro Solutions (Pty) Ltd
+# All rights reserved
+
+.WARRANTY
+    Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+    either express or implied. See the Apache-2.0 WITH Commons-Clause License for the specific language
+    governing permissions and limitations under the License.
+
 .SYNOPSIS
-    Checks current Authentication Methods Policy using REST API
+    Checks the current Authentication Methods Policy using REST API
 #>
 
 Write-Host "`n=== CURRENT AUTHENTICATION METHODS POLICY ===" -ForegroundColor Cyan
